abea950a67278a70b3489c15c99d85e2b31fdbec
Jonathan Simms <slyphon@gmail.com>
2020-03-05 17:44:18 -0500

    bump version to 0.99.0

--- Formula/libgit2.rb
+++ Formula/libgit2.rb
@@ -1,16 +1,16 @@
 class Libgit2 < Formula
   desc "C library of Git core methods that is re-entrant and linkable"
   homepage "https://libgit2.github.com/"
-  url "https://github.com/libgit2/libgit2/archive/v0.28.4.tar.gz"
-  sha256 "30f3877469d09f2e4a21be933b4e2800560d16646028dd800744dc5f7fb0c749"
+  url "https://github.com/libgit2/libgit2/archive/v0.99.0.tar.gz"
+  sha256 "174024310c1563097a6613a0d3f7539d11a9a86517cd67ce533849065de08a11"
   head "https://github.com/libgit2/libgit2.git"
 
-  bottle do
-    cellar :any
-    sha256 "87e6974b3a960f25b05f03f3e5d8e4cdbb280d637c98a9125d70f697e7668284" => :catalina
-    sha256 "50a790265b670987229ef99b4519926631f2faf6350f65fb0eaa8f19a8b40643" => :mojave
-    sha256 "709376382fec08680fae85ba1751ad8a2beb677a1cd24653636a242a3a228056" => :high_sierra
-  end
+  # bottle do
+  #   cellar :any
+  #   sha256 "87e6974b3a960f25b05f03f3e5d8e4cdbb280d637c98a9125d70f697e7668284" => :catalina
+  #   sha256 "50a790265b670987229ef99b4519926631f2faf6350f65fb0eaa8f19a8b40643" => :mojave
+  #   sha256 "709376382fec08680fae85ba1751ad8a2beb677a1cd24653636a242a3a228056" => :high_sierra
+  # end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
@@ -23,13 +23,10 @@ class Libgit2 < Formula
 
     mkdir "build" do
       system "cmake", "..", *args
+      system "make", "all"
       system "make", "install"
       cd "examples" do
-        (pkgshare/"examples").install "add", "blame", "cat-file", "cgit2",
-                                      "describe", "diff", "for-each-ref",
-                                      "general", "init", "log", "remote",
-                                      "rev-list", "rev-parse", "showindex",
-                                      "status", "tag"
+        (pkgshare/"examples").install "lg2"
       end
       system "make", "clean"
       system "cmake", "..", "-DBUILD_SHARED_LIBS=OFF", *args
